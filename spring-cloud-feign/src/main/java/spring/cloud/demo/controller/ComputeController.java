b18e6af0e45b9c6b073132fd80745906dc7d1b9d
zs <quicksandzn@gmail.com>
2017-02-28 14:25:15 +0800

    commit spring cloud

---
+++ spring-cloud-feign/src/main/java/spring/cloud/demo/controller/ComputeController.java
@@ -0,0 +1,18 @@
+package spring.cloud.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import spring.cloud.demo.client.ComputeClient;
+
+public class ComputeController {
+
+	@Autowired
+    ComputeClient computeClient;
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public Integer add() {
+        return computeClient.add(10, 20);
+    }
+    
+}
