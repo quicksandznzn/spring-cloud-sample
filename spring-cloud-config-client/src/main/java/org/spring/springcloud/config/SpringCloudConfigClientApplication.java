c0156f23abd525a4d4487e8f8c6a0d553037baa9
z n <quicksandzn@gmail.com>
2017-09-08 16:28:47 +0800

    init bus

--- spring-cloud-config-client/src/main/java/org/spring/springcloud/config/SpringCloudConfigClientApplication.java
+++ spring-cloud-config-client/src/main/java/org/spring/springcloud/config/SpringCloudConfigClientApplication.java
@@ -1,8 +1,8 @@
 package org.spring.springcloud.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class SpringCloudConfigClientApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringCloudConfigClientApplication.class, args);
+        new SpringApplicationBuilder(SpringCloudConfigClientApplication.class).web(true).run(args);
     }
 }
 
